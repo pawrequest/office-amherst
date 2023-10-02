5568eb6a9c125a302ea742b02a276c1f4d32297d
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-02 20:09:01 +0100

    set or check

--- asset_nums/assets.py
+++ asset_nums/assets.py
@@ -1,27 +1,60 @@
-from functools import partial
-
+from excel.excel import check_data, set_data
 import pandas as pd
 
-from excel.excel import check_data
 
-excel_path = r'C:\paul\office_am\fixtures\asset_example.xls'
+# input_file = r'C:\paul\office_am\fixtures\asset_example.xls'
+# sheet_name = 'Sheet1'
+# header_row = 2
+# output_file = r'C:\paul\office_am\fixtures\asset_example_out.xlsx'
+#
+# df = pd.read_excel(input_file, sheet_name=sheet_name, header=header_row)
+#
+# barcode_header = 'Barcode'
+# id_header = 'Number'
+# data_header = 'REPROG'
+# expected_value = 'Y'
+
+input_file = r'C:\paul\office_am\fixtures\asset_example.xls'
+output_file = r'C:\paul\office_am\fixtures\asset_example_out.xlsx'
 sheet_name = 'Sheet1'
 header_row = 2
+barcode_header = 'Barcode'
+id_header = 'Number'
+data_header = 'REPROG'
+expected_value = 'Y'
 
-df = pd.read_excel(excel_path, sheet_name=sheet_name, header=header_row)
+# Read Excel file
+df = pd.read_excel(input_file, sheet_name=sheet_name, header=header_row)
 
-# is_progged = partial(check_data, barcode_header='Barcode', id_header='Number', data_header='REPROG', expected_value='Y')
 
 
-def is_progged(df: pd.DataFrame, id_to_check: str | int):
-    return check_data(df, id_to_check, barcode_header='Barcode', id_header='Number', data_header='REPROG', expected_value='Y')
+def get_function_to_call():
+    while True:
+        choice = input("Set or check? (s/c)").lower()
+        if choice in ('s', 'c'):
+            return set_progged if choice == 's' else check_progged
+        print("Invalid option. Use 's' for set or 'c' for check.")
 
-while True:
-    id_to_check = str(input("Enter ID to check\n"))
-    try:
-        res = is_progged(df = df, id_to_check = id_to_check)
-    except ValueError as e:
-        print(e)
-        continue
-    print("yes" if res else "no")
+def set_progged(df: pd.DataFrame, id_to_handle: str | int, barcode_header: str, id_header: str, value_to_set: str | int | float):
+    col_to_set = id_header if len(id_to_handle) == 4 else barcode_header
+    set_data(df, id_to_set=id_to_handle, col_to_set=col_to_set, data_header=data_header, value_to_set=value_to_set)
+    df.to_excel(output_file, index=False)
+    print("Value set successfully.")
 
+def check_progged(df: pd.DataFrame, id_to_handle: str | int, barcode_header: str, id_header: str, value_to_set: str | int | float = None):
+    col_to_check = id_header if len(id_to_handle) == 4 else barcode_header
+    return check_data(df, id_to_check=id_to_handle, col_to_check=col_to_check, data_header=data_header, expected_value=expected_value)
+
+def set_or_check():
+    function_to_call = get_function_to_call()
+
+    while True:
+        id_to_handle = input("Enter ID\n")
+        try:
+            function_to_call(df, id_to_handle=id_to_handle, barcode_header=barcode_header, id_header=id_header, value_to_set=expected_value)
+        except ValueError as e:
+            print(f"No or multiple results found for {id_to_handle}.")
+        except PermissionError:
+            print("Close Excel and retry.")
+
+set_or_check()
--- excel/excel.py
+++ excel/excel.py
@@ -45,57 +45,35 @@ def edit_excel(infile: pathy, outfile: pathy, sheet: str, header_i: int, col_mat
         return 0
 
 
-def check_excel(workbook: pathy, data_to_check: str, sheet: str, col_with_name: str, col_with_data: str, header_i: int):
-    if isinstance(data_to_check, str):
-        data_to_check = [data_to_check]
-    for i, in_data in enumerate(data_to_check):
-        if os.path.isfile(in_data):
-            data_to_check[i] = os.path.splitext(os.path.basename(in_data))[0]
-
-    print(f"Checking {workbook} for {data_to_check}")
-    df = pd.read_excel(workbook, sheet_name=sheet, header=header_i)
-
-    for data in data_to_check:
-        row = df[df[col_with_name].str.upper() == data.upper()]
-        if len(row) != 1:
-            print(f"No or multiple results found for {data}")
-            input("Press enter to continue...")
-            continue
-        actual_data = row[col_with_data].iloc[0]
-        print(f"Value for {data} is {actual_data}")
-        input("Press enter to exit...")
+# def check_data(df: pd.DataFrame, id_to_check: Union[str, int], col_to_check:str,
+#                data_header: str, expected_value: Union[str, int, float]) -> bool:
+#
+#     row = df[df[col_to_check].astype(str) == str(id_to_check)]
+#     if len(row) != 1:
+#         raise ValueError(f"No or multiple results found for {id_to_check}")
+#     return expected_value == row[data_header].iloc[0]
+#
+# def set_data(df: pd.DataFrame, id_to_set: str | int, col_to_set: str, data_header: str, value_to_set: str | int | float):
+#     index_to_set = df[df[col_to_set].astype(str) == str(id_to_set)].index
+#     if len(index_to_set) != 1:
+#         raise ValueError(f"No or multiple results found for {id_to_set}")
+#     df.at[index_to_set[0], data_header] = value_to_set
+#
+#     return True
 
 
-def check_excel_file2(df: pd.DataFrame, target_data, name_col, data_col):
-    # Convert single string to list for uniform handling
-    target_data = [target_data] if isinstance(target_data, str) else target_data
 
-    for data in target_data:
-        row = df[df[name_col].str.upper() == data.upper()]
-
-        if len(row) != 1:
-            input(f"No or multiple results found for {data} - Press enter to continue to next value.")
-            continue
-
-        actual_data = row[data_col].iloc[0]
-        print(f"Value for {data} is {actual_data}")
-
-    input("Press enter to exit...")
-
-
-def is_progged(df: pd.DataFrame, id_to_check: str | int, barcode_header: str, id_header: str, reprogged_header: str,
-               reprogged_tag: str):
-    col_to_check = id_header if len(str(id_to_check)) == 4 else barcode_header
-    row = df[df[col_to_check].astype(str) == str:id_to_check]
-    if len(row) != 1:
-        raise ValueError(f"No or multiple results found for {id}")
-    return reprogged_tag == row[reprogged_header].iloc[0]
-
-def check_data(df: pd.DataFrame, id_to_check: Union[str, int], barcode_header: str, id_header: str,
-               data_header: str, expected_value: Union[str, int, float]) -> bool:
-
-    col_to_check = id_header if len(str(id_to_check)) == 4 else barcode_header
+def check_data(df: pd.DataFrame, id_to_check: str | int, col_to_check:str, data_header: str, expected_value: str | int | float) -> bool:
     row = df[df[col_to_check].astype(str) == str(id_to_check)]
     if len(row) != 1:
-        raise ValueError(f"No or multiple results found for {id_to_check}")
-    return expected_value == row[data_header].iloc[0]
+        raise ValueError()
+    res = expected_value == row[data_header].iloc[0]
+    print('yes' if res else 'no')
+    return res
+
+def set_data(df: pd.DataFrame, id_to_set: str | int, col_to_set: str, data_header: str, value_to_set: str | int | float):
+    index_to_set = df[df[col_to_set].astype(str) == str(id_to_set)].index
+    if len(index_to_set) != 1:
+        raise ValueError()
+    df.at[index_to_set[0], data_header] = value_to_set
+    return True

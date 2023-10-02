2b31d0c94b4670a8cc9e3c017fecf24b6e3ca4e9
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-02 21:49:34 +0100

    delete old code

--- asset_nums/assets.py
+++ asset_nums/assets.py
@@ -1,67 +1,6 @@
 import pandas as pd
 
 from excel.excel import check_data, set_data
-#
-# INPUT_FILE = r'C:\paul\office_am\fixtures\asset_example.xls'
-# OUTPUT_FILE = r'C:\paul\office_am\fixtures\asset_example_out.xlsx'
-# SHEET_NAME = 'Sheet1'
-# HEADER_ROW = 2
-#
-# BARCODE_HEADER = 'Barcode'
-# ID_HEADER = 'Number'
-# DATA_HEADER = 'REPROG'
-# EXPECTED_VALUE = 'Y'
-#
-#
-# def get_excel(in_file=INPUT_FILE, sheet=SHEET_NAME, headers=HEADER_ROW):
-#     return pd.read_excel(in_file, sheet_name=sheet, header=headers)
-#
-#
-# def get_function_to_call():
-#     while True:
-#         choice = input("Set or check? (s/c)").lower()
-#         if choice in ('s', 'c'):
-#             return set_progged if choice == 's' else check_progged
-#         print("Invalid option. Use 's' for set or 'c' for check.")
-#
-#
-# def set_progged(id_to_handle: str | int, df: pd.DataFrame = None, barcode_header: str = BARCODE_HEADER,
-#                 id_header: str = ID_HEADER, data_header: str = DATA_HEADER,
-#                 value: str | int | float = EXPECTED_VALUE):
-#     if df is None:
-#         df = get_excel()
-#     col_to_set = id_header if len(id_to_handle) == 4 else barcode_header
-#     set_data(df, id_to_set=id_to_handle, col_to_set=col_to_set, data_header=data_header, value_to_set=value)
-#     df.to_excel(OUTPUT_FILE, index=False)
-#     print("Excel sheet successfully updated.")
-#
-#
-# def check_progged(id_to_handle: str | int, df: pd.DataFrame = None, barcode_header: str = BARCODE_HEADER,
-#                   id_header: str = ID_HEADER, data_header: str = DATA_HEADER,
-#                   value: str | int | float = EXPECTED_VALUE):
-#     if df is None:
-#         df = get_excel()
-#     col_to_check = id_header if len(id_to_handle) == 4 else barcode_header
-#     return check_data(df, id_to_check=id_to_handle, col_to_check=col_to_check, data_header=data_header,
-#                       expected_value=value)
-#
-#
-# def set_or_check():
-#     function_to_call = get_function_to_call()
-#     while True:
-#         id_to_handle = input("Enter ID\n")
-#         try:
-#             function_to_call(id_to_handle=id_to_handle, barcode_header=BARCODE_HEADER,
-#                              id_header=ID_HEADER, value=EXPECTED_VALUE)
-#         except ValueError:
-#             print(f"No or multiple results found for {id_to_handle}.")
-#         except PermissionError:
-#             print("Close Excel and retry.")
-#
-#
-# if __name__ == "__main__":
-#     set_or_check()
-
 
 INPUT_FILE = r'C:\paul\office_am\fixtures\asset_example.xls'
 OUTPUT_FILE = r'C:\paul\office_am\fixtures\asset_example_out.xlsx'

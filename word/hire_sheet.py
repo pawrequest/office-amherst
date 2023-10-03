0209adce88f61a30f8c4c4f18ca0d976a7eb2928
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-03 19:55:04 +0100

    hire sheet argparse - commence agent works

--- word/hire_sheet.py
+++ word/hire_sheet.py
@@ -1,21 +1,36 @@
+import argparse
+import os
+
 from dbfread import DBF
 
 from docxtpl import DocxTemplate
 
+HIRE_TEMPLATE = r'C:\paul\office_am\fixtures\hire_sheet_tmplt.docx'
+HIRE_DBF_EXAMPLE = r'C:\paul\office_am\fixtures\hire_sheet.dbf'
+HIRE_SHEET_OUTPUT = r'C:\paul\office_am\fixtures\hire_sheet_output2.docx'
+def amherst_doc(template = HIRE_TEMPLATE, dbf= HIRE_DBF_EXAMPLE, output=HIRE_SHEET_OUTPUT):
+    hire = [i for i in DBF(dbf, load=True, encoding='cp1251')][0]
 
-def main():
-    hire = [i for i in DBF('../fixtures/hire_sheet.dbf', load=True, encoding='cp1251')][0]
-
-    hire_template = r'C:\paul\office_am\fixtures\hire_sheet_tmplt.docx'
     context = {k: v for k, v in hire.items()}
     for k, v in context.items():
         if isinstance(v, str):
             context[k] = v.split('\x00')[0]
 
-    doc = DocxTemplate(hire_template)
+    doc = DocxTemplate(template)
     doc.render(context)
-    doc.save("generated_doc.docx")
 
+    doc.save(output)
+    os.startfile(str(output), "print")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate Amherst Doc')
+    parser.add_argument('--template', default=HIRE_TEMPLATE, help='Path to template file')
+    parser.add_argument('--dbf', default=HIRE_DBF_EXAMPLE, help='Path to DBF file')
+    parser.add_argument('--output', default=HIRE_SHEET_OUTPUT, help='Path to output file')
+
+    args = parser.parse_args()
+    amherst_doc(args.template, args.dbf, args.output)
 
 if __name__ == '__main__':
     main()

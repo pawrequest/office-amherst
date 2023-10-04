3de7ffdcc24c1ac47c16eee592460c53042379ca
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-04 15:43:11 +0100

    get connected works

--- word/invoice_dde.py
+++ word/invoice_dde.py
@@ -1,37 +1,47 @@
+from dataclasses import dataclass
 from typing import Iterable
 
 import win32com.client
 
 
-def speak_to_commence(agent_trigger, category, command):
-    cmc = win32com.client.Dispatch("Commence.DB")
-    conv = cmc.GetConversation("Commence", "GetData")
-    dde = f"[FireTrigger({agent_trigger}, {category}, {command})]"
-    conv.Execute(dde)
+def fire_commence_agent(agent_trigger, category, command):
+    conv = get_conversation()
+    conv.Execute(f"[FireTrigger({agent_trigger}, {category}, {command})]")
 
 
-def speak_to_commence2():
-    cmc = win32com.client.Dispatch("Commence.DB")
-    conv = cmc.GetConversation("Commence", "ViewData")
-    dde = f"ViewCategory(Customer)"
-    conv.Execute(dde)
-    dde2 = r"ViewFilter(1, F,,Name, Equal to, Test,)"
-    conv.Execute(dde2)
-
-    ...
+@dataclass
+class Connection:
+    name: str
+    table: str
+    fields: Iterable[str]
 
 
-def do_commence(table, name, fields: Iterable[str]):
+def do_commence(table, name, fields: Iterable[str], connections: Iterable[Connection] = None):
+    results = {}
     conv = get_conversation()
     conv = get_record(conv, table, name)
-    data = get_data(conv, fields)
-    return data
+    results['data'] = get_data(conv, fields)
+    if connections:
+        results['connected_data'] = {}
+        for connection in connections:
+            connected_data = get_connected_data(conv, connection)
+            results['connected_data'][connection.name] = connected_data
+        ...
+    return results
+
+
+def get_connected_data(conv, connection: Connection):
+    connected_name = conv.Request(f"[ViewConnectedItem(1, {connection.name}, {connection.table}, 1)]")
+    connected_conv = get_record(conv, connection.table, connected_name)
+    connected_data = get_data(connected_conv, connection.fields)
+    return connected_data
 
 
 def get_conversation():
     cmc = win32com.client.Dispatch("Commence.DB")
     conv = cmc.GetConversation("Commence", "GetData")
     return conv
+
 
 def get_record(conv, table, name):
     conv.Request(f"[ViewCategory({table})]")
@@ -41,28 +51,25 @@ def get_record(conv, table, name):
         raise ValueError(f"{item_count} entries found")
     return conv
 
+
 def get_data(conv, fields: Iterable[str]):
     return {field: conv.Request(f"[ViewField(1, {field})]") for field in fields}
 
-def get_connected_data(conv, connection, fields: Iterable[str]):
-    ...
 
-
-
-
-
-
-fields_to_get = [
+fields_to_get_hire = ["Name"]
+fields_to_get_cust = [
     "Telephone",
 ]
+hires_to = Connection(name="Has Hired", table='Hire', fields=fields_to_get_hire)
+
 # Usage
-customer_data = do_commence(table="Customer", name="Test", fields=fields_to_get)
+customer_data = do_commence(table="Customer", name="Test", fields=fields_to_get_cust, connections=[hires_to])
 # speak_to_commence2()
 ...
 
 
 def display_test_customer():
-    speak_to_commence(agent_trigger='PYTHON_DDE', category='Customer', command='Test')
+    fire_commence_agent(agent_trigger='PYTHON_DDE', category='Customer', command='Test')
 
 
 display_test_customer()

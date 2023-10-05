adcadea873ef1643139bc0beba609072110bcd3c
Paul <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-05 02:57:40 +0100

    test fixtures

--- tests/test_dde.py
+++ tests/test_dde.py
@@ -11,6 +11,14 @@ from tmplt.entities import Connection, HirePrice, HireProduct, Price, SaleProduc
 def conv():
     return get_conversation()
 
+@pytest.fixture
+def hire_name():
+    return 'Trampoline League - 27/06/2023 ref 31247'
+
+@pytest.fixture
+def sale_name():
+    return 'Truckline Services - 26/10/2022 ref 11'
+
 def test_get_conv():
     conv = get_conversation()
     assert conv is not None
@@ -37,15 +45,12 @@ def test_customer_data():
     sales_to = Connections.CUSTOMER_SALES.value
     customer_data = get_commence_data(table="Customer", name="Test", fields=Fields.CUSTOMER.value,
                                       connections=[hires_to, sales_to])
-    assert all(field in customer_data['Customer'] for field in
-               Fields.CUSTOMER.value), f"Missing fields in customer_data['Customer']"
+    assert all(field in customer_data['Customer'] for field in Fields.CUSTOMER.value)
     for hire_record in customer_data['Hire'].values():
-        assert all(field in hire_record for field in
-                   Fields.HIRE.value), f"Missing fields in a Hire record"
+        assert all(field in hire_record for field in Fields.HIRE.value)
 
     for sale_record in customer_data['Sale'].values():
-        assert all(field in sale_record for field in
-                   Fields.SALE.value), f"Missing fields in a Sale record"
+        assert all(field in sale_record for field in Fields.SALE.value)
 
 def test_wrong_customer_name():
     with pytest.raises(ValueError):
@@ -56,18 +61,16 @@ def test_get_hire():
     some_hire = get_data_generic(some_hire_name, 'Hire')
     assert some_hire['Hire']['Name'] == some_hire_name
 
-def test_get_sale():
-    some_sale_name = 'Truckline Services - 26/10/2022 ref 11'
-    some_sale = get_data_generic(some_sale_name, 'Sale')
-    assert some_sale['Sale']['Name'] == some_sale_name
+def test_get_sale(sale_name):
+    # some_sale_name = 'Truckline Services - 26/10/2022 ref 11'
+    some_sale = get_data_generic(sale_name, 'Sale')
+    assert some_sale['Sale']['Name'] == sale_name
 
-def test_get_generic():
-    some_hire_name = 'Trampoline League - 27/06/2023 ref 31247'
-    some_hire = get_data_generic(some_hire_name, 'Hire')
-    assert some_hire['Hire']['Name'] == some_hire_name
-    some_sale_name = 'Truckline Services - 26/10/2022 ref 11'
-    some_sale = get_data_generic(some_sale_name, 'Sale')
-    assert some_sale['Sale']['Name'] == some_sale_name
+def test_get_generic(hire_name, sale_name):
+    some_hire = get_data_generic(hire_name, 'Hire')
+    assert some_hire['Hire']['Name'] == hire_name
+    some_sale = get_data_generic(sale_name, 'Sale')
+    assert some_sale['Sale']['Name'] == sale_name
 
 
 

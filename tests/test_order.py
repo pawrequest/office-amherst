adae8159bd663a9824a150721e6d913e7f0626fb
Paul <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2023-10-20 22:56:16 +0100

    moved static files

--- tests/test_order.py
+++ tests/test_order.py
@@ -11,24 +11,6 @@ from office_am.order.order_ent import LineItem
 from office_am.order.transact import TransactionContext
 
 
-#
-# def test_libre():
-#     ot = OfficeTools.libre()
-#     temp_dir = tempfile.mkdtemp()
-#     outfile = Path(temp_dir) / "outfile.docx"
-#
-#     with CmcContext() as cmc:
-#         hires = cmc.hires_by_customer('Test')
-#         hire = hires[0]
-#
-#         with TransactionContext() as tm:
-#             hire_inv = tm.get_hire_invoice(hire)
-#             assert isinstance(hire_inv.order.line_items[0], LineItem)
-#             template, temp_file_ = get_inv_temp(hire_inv)
-#             shutil.copy(temp_file_, outfile)
-#             pdf_file = ot.pdf.from_docx(outfile)
-#             assert pdf_file.is_file()
-
 
 @pytest.mark.parametrize("office_tool_method", [OfficeTools.microsoft, OfficeTools.libre])
 def test_office_tools(office_tool_method):
